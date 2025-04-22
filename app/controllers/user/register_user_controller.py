300036e01a56c5c2b2694deb8abda7099ecbfdd2
BradleyParkerDev <bradleyscottparker@outlook.com>
2025-04-22 00:17:44 -0500

    added logging to web routes

--- app/controllers/user/register_user_controller.py
+++ app/controllers/user/register_user_controller.py
@@ -1,6 +1,11 @@
 import json
+from app.lib.logger import get_logger
 from fastapi import Request, Response, HTTPException, status
 
 # register user controller
 async def register_user_controller(request:Request, response:Response):
-    return {"message": "User successefully registerd!!!"}
+    logger = get_logger("user")
+    logger.info("User successefully registered!!!")
+    request_body = await request.json()
+    print(request_body['first_name'])
+    return {"message": "User successefully registered!!!"}
--- app/controllers/web/authenticated_user_page_controller.py
+++ app/controllers/web/authenticated_user_page_controller.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from app.lib.logger import get_logger
 from fastapi import Request, Response, HTTPException, status
 from fastapi.templating import Jinja2Templates 
 from app.lib import LayoutUtility
@@ -15,6 +16,9 @@ layout = LayoutUtility()
 
 
 async def authenticated_user_page_controller(request:Request, response:Response, id:str):
+
+    logger = get_logger("web")
+    logger.info("User accessed authenticated user page!!!")
 
     heading = "This is a authenticated user page!!!"
     message = (f"User Name:{id}")
